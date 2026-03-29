c6fc54751bb46fe044daaaaaf810f3531c130c2c
Proxay <ayden.melhop@proxay.com.au>
2026-03-29 00:53:10 +0000

    Settup lldb debugger for Crystal

--- src/playground.cr
+++ src/playground.cr
@@ -3,4 +3,17 @@ module Playground
   VERSION = "0.1.0"
 
   # TODO: Put your code here
+
+  class Greeter
+    def initialize(@name : String )
+    end
+
+    def salute
+      	puts "Hello #{@name}!"
+    end
+  end
+
+  g = Greeter.new("world")
+  g.salute
+
 end
